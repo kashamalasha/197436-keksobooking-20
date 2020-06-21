6fde169937e3cc9ee0b1371999bf153ff90c2ea4
kashamalasha <dmitry.burnyshev@gmail.com>
2020-06-21 14:56:00 +0300

    Исправляет опечатку.

--- js/main.js
+++ js/main.js
@@ -199,7 +199,7 @@ var renderOffer = function (obj) {
 
   renderText(offer.querySelector('.popup__type'),
       obj.offer.type,
-      placement.fromId(obj.offer.type).name);
+      Placement.fromId(obj.offer.type).name);
 
   renderText(offer.querySelector('.popup__text--capacity'),
       obj.offer.rooms && obj.offer.guests,
