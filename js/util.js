f563fb66fea081c18e54a59bd17cd86073392e79
kashamalasha <dmitry.burnyshev@gmail.com>
2020-07-24 15:01:02 +0300

    Добавляет getShuffledArray() в util.js

--- js/util.js
+++ js/util.js
@@ -31,6 +31,21 @@
     return randomArray;
   };
 
+  var getShuffledArray = function (arr) {
+    var j;
+    var x;
+    var i;
+
+    for (i = arr.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = arr[i];
+      arr[i] = arr[j];
+      arr[j] = x;
+    }
+
+    return arr;
+  };
+
   var isEscEvent = function (evt, action) {
     if (evt.keyCode === Keys.ESCAPE.keyCode) {
       evt.preventDefault();
@@ -49,7 +64,8 @@
     getRandomArray: getRandomArray,
     getRandomFromArray: getRandomFromArray,
     isEscEvent: isEscEvent,
-    isEnterEvent: isEnterEvent
+    isEnterEvent: isEnterEvent,
+    getShuffledArray: getShuffledArray
   };
 
 })();
